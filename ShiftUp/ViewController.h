e6929ca9fa9ea7dbebe6c2d53644fd625af8e4f3
bitey <bitey@outlook.com>
2013-04-10 15:58:14 -0400

    Initial Commit

---
+++ ShiftUp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShiftUp
+//
+//  Created by StopBitingMe on 4/10/13.
+//  Copyright (c) 2013 AFGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ShiftUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShiftUp
+//
+//  Created by StopBitingMe on 4/10/13.
+//  Copyright (c) 2013 AFGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
